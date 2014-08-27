268b7547a5d6cec00c377a8722a4060ee1e811e0
Sam Sharp <sharpsam87@gmail.com>
2014-08-27 14:33:39 +0100

    refactored into seperate function

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ function getPage(url, callback) {
 	request.get(url, callback)
 }
 
-var url = 'https://en.wikipedia.org/wiki/List_of_social_and_political_philosophers'
-
-getPage(url, function(err,resp,body){
+function  parsePhilosopherLinks(err,resp,body) {
 	if (err) {
 		console.log('had an error');
 		console.log(err);
 		return;
-	}
+	};
 	var $ = cheerio.load(body);
-	var li = $('h2:contains(Alphabetical)').next().find('li')
-	$(li).each(function(index, item) {
+	$($('h2:contains(Alphabetical)').next().find('li')).each(function(index, item) {
 		console.log($(item).text());
+		return;
 	});
-});
+};
+	
+
+var url = 'https://en.wikipedia.org/wiki/List_of_social_and_political_philosophers'
+
+getPage(url, parsePhilosopherLinks);
+
