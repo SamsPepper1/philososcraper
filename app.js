04824169b9bd62787937f391d5e6915af5de14e4
Sam Sharp <sharpsam87@gmail.com>
2014-08-27 15:03:17 +0100

    added function to print bith, death and influences/influenced

--- app.js
+++ app.js
@@ -21,8 +21,28 @@ function  parsePhilosopherLinks(body) {
 	});
 };
 	
+function getPhilosopherInfo(name, callback) {
+	var url = 'https://en.wikipedia.org/wiki/' + name.replace(' ','_');
+	request.get(url, function(err, resp, body) {
+		if (err){
+			console.log('error finding philosophers page');
+			return;
+		}
+		var queryString = ['Born','Died','Influences','Influenced'].map(function(item){
+			return 'th:contains(' + item + ')'
+		}).join();
+		$ = cheerio.load(body);
+		var infobox = $('table.infobox');
+		$(infobox).find(queryString).each(function(index, item){
+			console.log($(item).text());
+			console.log($(item).next().text().trim() + '\r\n');
+			return;
+		});
+		return;
+	});
+	return;	
+}
 
-var url = 'https://en.wikipedia.org/wiki/List_of_social_and_political_philosophers'
-
-getPage(url, parsePhilosopherLinks);
-
+//var url = 'https://en.wikipedia.org/wiki/List_of_social_and_political_philosophers'
+//getPage(url, parsePhilosopherLinks);
+getPhilosopherInfo('Karl Marx', function(){});
