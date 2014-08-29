77cf29648d10e76e6d68fd184e54b39c2b26ec77
Sam Sharp <sharpsam87@gmail.com>
2014-08-29 21:45:47 +0100

    added test client app

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var runScraper = require('./scraper').runScraper;
 
 var app = express();
 
+
+var path_to_here = require('path').dirname(require.main.filename);
 app.get('/testing', function(req,res){
 	res.send('1,2,3...');
 });
+app.get('/test_app', function(req, res){
+	res.sendfile(path_to_here + '/public/index.html');	
+})
 
 
 app.get('/run_scraper/', runScraper);
