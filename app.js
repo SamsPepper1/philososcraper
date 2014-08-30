b29f75d615de705947d3543010b564e83d1c642e
Sam Sharp <sharpsam87@gmail.com>
2014-08-30 14:54:12 +0100

    using epress with jade templates

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ var runScraper = require('./scraper').runScraper;
 
 var app = express();
 
+app.set('post', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', {layout: false});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/testing', function(req,res){
 	res.send('1,2,3...');
 });
 app.get('/test_app', function(req, res){
-	res.sendFile(path.join(__dirname ,'/public/index.html'));	
+	res.render('index', {
+		title: 'TITLE'
+	});
 })
 
 
