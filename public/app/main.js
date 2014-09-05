ed0f4d7f3a5169a2092a3657352df0b9260bcc94
Sam Sharp <sharpsam87@gmail.com>
2014-09-05 18:31:31 +0100

    added label layer and year markers

--- public/app/main.js
+++ public/app/main.js
@@ -1,6 +1,6 @@
 (function(){
 	var app = angular.module('philososcraper', []);
-
+	
 	app.controller('philListController', ['$http', '$scope',function($http, $scope){
 		this.title = 'list of philosophers';
 		var controller = this;
@@ -17,14 +17,47 @@
 				philosophers: '='
 				},
 			link: function(scope, element, attrs) {
+				// make svg and main graph layer
 				var graph = d3.select(element[0])
 					.append('svg')
 					.attr("width", 3000)
 					.attr("height", 3000)
 					.append('g');
 
+				//create label layer
+				var labels = d3.select('svg').append('g');
+
+
+				function removeLabels() {
+					labels.selectAll('text').remove();
+					return;
+				};
+				// make x axis
+				var years = [];
+				for (var i = -500; i < 2020; i += 100){
+					years.push(i);
+				}
+				var point = graph.selectAll('.label')
+					.data(years)
+					.enter().append('g');
+				point.append('rect')
+					.attr("x", 0)
+					.attr("y", function(d) { return d +500})
+					.attr("height",5)
+					.attr("width", 3000)
+					.style("fill", "blue")
+
+				point.select('rect').on("mouseover", function(d) {
+					labels.append("text")
+						.attr("dx", d3.event.pageX)
+						.attr("dy", d + 510)
+						.text(yearToString(d))
+					return;
+				})
+				point.select('rect').on("mouseout",removeLabels); 				
+
 				scope.$watch('philosophers',function(newVal, oldVal) {
-					graph.selectAll("*").remove();
+					graph.selectAll(".philosopher").remove();
 					var node = graph.selectAll(".philosopher")
 					.data(scope.philosophers)
 					.enter().append('g')
@@ -37,22 +70,26 @@
 					.attr("x", function(d,i){ return 5*i})
 					.attr("y", function(d,i){ return d.born + 500})
 
-					node.on("mouseover", function() {
-						d3.select(this).append('svg:text')
+					node.on("mouseover", function(d) {
+						labels.append('svg:text')
 						.style("fill", "black")
-						.attr("dx", function(d,i){ return 5*i})
-						.attr("dy",function(d,i) { return d.born + 515})
-						.text(function(d,i){ return d.name + "lived from " + d.born + " to " + d.died + "."});
+						.attr("dx", function(){ return d3.event.clientX})
+						.attr("dy",function() { return d.born + 515})
+						.text(function(){ return d.name + "lived from " + yearToString(d.born) + " to " + yearToString(d.died) + "."});
 					});
 					
-					node.on("mouseout", function() {
-						d3.select(this).select("text").remove();
-					});
+					node.on("mouseout",removeLabels);
 					return;
 				});
 				return;
 			},
 		}
 	});
+
+	function yearToString(year) {
+		var s = Math.abs(year).toString();
+		s += year >= 0? ' AD': ' BC';
+		return s;
+	}
 
 })();
